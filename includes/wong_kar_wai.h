665579d4c28f2b2176a5a6cbac13a7a661261bff
LuckyIntegral <frantsv2004@gmail.com>
2024-05-11 14:33:10 +0200

    fix identation issue

--- includes/wong_kar_wai.h
+++ includes/wong_kar_wai.h
@@ -6,7 +6,7 @@
 /*   By: vfrants <vfrants@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 13:34:40 by vfrants           #+#    #+#             */
-/*   Updated: 2024/05/11 13:36:20 by vfrants          ###   ########.fr       */
+/*   Updated: 2024/05/11 14:28:27 by vfrants          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,34 @@
 
 # include "../libft/libft.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <time.h>
 
-#ifndef SIZE
-# define SIZE 4
-#endif
+# ifndef SIZE
+#  define SIZE 4
+# endif
 
-#define WIN_VALUE 2048
+enum e_const
+{
+    WIN_VALUE = 2048
+};
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_WHITE   "\x1b[37m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+# define ANSI_COLOR_RED     "\x1b[31m"
+# define ANSI_COLOR_GREEN   "\x1b[32m"
+# define ANSI_COLOR_YELLOW  "\x1b[33m"
+# define ANSI_COLOR_BLUE    "\x1b[34m"
+# define ANSI_COLOR_MAGENTA "\x1b[35m"
+# define ANSI_COLOR_CYAN    "\x1b[36m"
+# define ANSI_COLOR_WHITE   "\x1b[37m"
+# define ANSI_COLOR_RESET   "\x1b[0m"
+
+typedef enum e_direction
+{
+    GAME_WON,
+    GAME_LOST,
+    GAME_CONTINUE,
+}	t_direction;
 
 #endif // WONG_KAR_WAI_H
